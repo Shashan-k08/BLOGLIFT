0859b5d9edccdd53d1695362be29e339585ad970
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-06-13 22:37:06 +0530

    nav component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 import * as React from 'react';
+import Navbar from '../components/Navbar';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <>
+  <Navbar/>
+   <Component {...pageProps} /> </>
 }
